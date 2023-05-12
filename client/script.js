093bb250bc00714cb4311ede9dcd28660508aa81
MicheMonfort95 <120752960+MicheMonfort95@users.noreply.github.com>
2023-05-12 13:08:01 -0400

    Update script.js

--- client/script.js
+++ client/script.js
@@ -81,7 +81,7 @@ const handleSubmit = async (e) => {
 
   //fetch data from server get bot response
 
-  const response = await fetch("https://alfred-your-butler-ai.onrender.com", {
+  const response = await fetch("http://localhost:5000", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
